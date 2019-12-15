25169190e809e5d5008544af62d80ed04ee39c70
shane <2206907681@qq.com>
2019-12-15 15:20:57 +0800

    对照笔记优化2之前

---
+++ event/web_back/tools/config.js
@@ -0,0 +1,8 @@
+// 此文件配置所有的接口信息
+
+var BASE_URL = 'http://localhost:8000';
+var LOGIN = BASE_URL + '/admin/login';
+var LOGOUT = BASE_URL + '/admin/logout';
+var GET_USER = BASE_URL + '/admin/getuser';
+var ARTICLE_COUNT = BASE_URL + '/admin/article_count';
+var COMMENT_COUNT = BASE_URL + '/admin/comment_count';
